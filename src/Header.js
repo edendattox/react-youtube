c1943a9ed700cf7be67493278be40cf02d20a2d3
rahul kathait <edendattox@gmail.com>
2021-02-20 19:09:33 +0530

    making header

---
+++ src/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./Header.css"
+
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import { Avatar } from '@material-ui/core';
+
+
+function Header() {
+    return (
+    
+<div className="header">
+
+    <div className="header__left">
+     <MenuIcon/>
+        <img 
+        className="header__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+        alt="#"/>
+    </div>
+    
+    <div className="header__input">
+    <input type="text"/>
+   <SearchIcon/>
+    </div>
+    
+   <div className="header__icons">
+    <VideoCallIcon />
+    <AppsIcon />
+    <NotificationsIcon />
+    <Avatar 
+     alt="eden"
+     src="https://i.pinimg.com/564x/bb/0b/d0/bb0bd0e0477fadc41393c7f64571e321.jpg"
+    />
+   </div>
+    
+</div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// var firebaseConfig = {
+//   apiKey: "AIzaSyADfoeLcLHA50tURLrLhv7Wrn0Ndu6Hxck",
+//   authDomain: "react--clone-c54d3.firebaseapp.com",
+//   projectId: "react--clone-c54d3",
+//   storageBucket: "react--clone-c54d3.appspot.com",
+//   messagingSenderId: "291309868814",
+//   appId: "1:291309868814:web:ca1c127b3bce139755295f",
+//   measurementId: "G-QC61RB1P20"
+// };
+// // Initialize Firebase
